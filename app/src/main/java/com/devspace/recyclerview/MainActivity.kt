db61aa48c53c4d705a312a33b8711fef1216c365
lucas <lucas.cavalcante0013@gmail.com>
2024-06-26 16:48:49 -0300

    80% completed

--- app/src/main/java/com/devspace/recyclerview/MainActivity.kt
+++ app/src/main/java/com/devspace/recyclerview/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.devspace.recyclerview
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.contracts.contract
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,38 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val ivList = findViewById<ImageView>(R.id.iv_list)
+        val ivGrid = findViewById<ImageView>(R.id.iv_grid)
+        val rvList = findViewById<RecyclerView>(R.id.rv_list)
+        val adapter = ContactListAdapter()
+
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+        adapter.submitList(Contacts)
+
+        ivGrid.setOnClickListener(){
+            rvList.layoutManager = GridLayoutManager(this,2)
+
+        }
+        ivGrid.setOnClickListener(){
+            rvList.layoutManager = LinearLayoutManager(this)
+
+
+        }
+        adapter.setOnClickListener { Contact ->
+
+        }
+
     }
+
 }
+private val Contacts = listOf( Contact(
+  name = "Lucas",
+    phone = "(55) 61 95482486",
+    icon = R.drawable.sample9
+),
+)
+
+
+
